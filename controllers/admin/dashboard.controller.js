19a0d5b9f16369d59b10d9c62e6973c0a40814f5
Nguyen Duc Huy <huyn0634@gmail.com>
2024-11-01 21:25:55 +0700

    Update page dashboard admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -2,7 +2,8 @@ const Product = require("../../models/product.model");
 const Account = require("../../models/account.model");
 const ProductCategory = require("../../models/product-category.model");
 const User = require("../../models/user.model");
-module.exports.dashboard = async(req, res) => {
+const Order = require("../../models/order.model");
+module.exports.dashboard = async (req, res) => {
     const statistic = {
         categoryProduct: {
             total: 0,
@@ -23,6 +24,15 @@ module.exports.dashboard = async(req, res) => {
             total: 0,
             active: 0,
             inactive: 0,
+        },
+        order: {
+            total: 0,
+            approved: 0,
+            initial: 0,
+            processing: 0,
+            delivered: 0,
+            shipping: 0,
+            stopShipping: 0,
         },
     };
     //Get product category
@@ -56,6 +66,27 @@ module.exports.dashboard = async(req, res) => {
     });
     statistic.user.inactive = await User.countDocuments({
         status: "inactive",
+    });
+
+    //Get user
+    statistic.order.total = await Order.countDocuments({});
+    statistic.order.approved = await Order.countDocuments({
+        status: "approved",
+    });
+    statistic.order.initial = await Order.countDocuments({
+        status: "initial",
+    });
+    statistic.order.processing = await Order.countDocuments({
+        statusDelivery: "processing",
+    });
+    statistic.order.shipping = await Order.countDocuments({
+        statusDelivery: "shipping",
+    });
+    statistic.order.delivered = await Order.countDocuments({
+        statusDelivery: "delivered",
+    });
+    statistic.order.stopShip = await Order.countDocuments({
+        statusDelivery: "stop-shipping",
     });
 
     res.render("admin/pages/dashboard/index", {
